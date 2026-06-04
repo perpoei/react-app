7fc89c03ee4a08727176086dc436ffe3ef5455c9
perpoei <1007456293@qq.com>
2026-06-05 01:48:06 +0800

    feat:  首页页面布局编写以及引入tailwindCss写法和适配

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/App.tsx'
+import '@/index.css'
+import '@/styles/common.css'
+import 'tailwindcss'
 import 'react-vant/lib/index.css'
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
+import { NoticeBar } from 'react-vant';
+import { InfoO } from '@react-vant/icons'
+import  '@/styles/Home/index.css';
+
 export default function Chat() {
     return (
-        <div style={{ padding: '20px' }}>
-            <h1>首页</h1>
+        <div className='container'>
+            <NoticeBar leftIcon={<InfoO />} scrollable text="基于AI的智能景点介绍与行程规划系统" />
+            <div className="card search-card">
+                <h3>规划你的旅程</h3>
+            </div>
+            <div className="card"></div>
+            <div className="card"></div>
         </div>
     )
 }
